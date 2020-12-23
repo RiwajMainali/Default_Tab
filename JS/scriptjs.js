160a9ccd55eca10641de46ecc5239d8e0c0e3ca8
Riwaj Mainali <riwaj227@gmail.com>
2020-12-23 14:13:25 -0600

    Tidying up youtube search and adding search engine

--- JS/scriptjs.js
+++ JS/scriptjs.js
@@ -3,9 +3,27 @@ function ysearch(){
     
     
     b=document.getElementById("cl").value;
+    if(b.length==0)
+    {
+        window.open("https://www.youtube.com")
+    }
+    else{
+        c=b.replace(" ","+");
+    
+        a="https://www.youtube.com/results?search_query="+c;
+        window.open(a);
+    
+    }
+
+}
+function wsearch(){
+    
+    
+    
+    b=document.getElementById("webinput").value;
     c=b.replace(" ","+");
     
-    a="https://www.youtube.com/results?search_query="+c;
+    a="https://duckduckgo.com/?q="+c+"&t=chromentp&atb=v239-1&ia=web";
     window.open(a);
 
 }
